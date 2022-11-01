3c832cfa8bf33715fa9bc6ab0b6f7165ea2daf3b
SooHwan <newpoo@naver.com>
2022-11-01 20:15:39 +0900

    refactor: Record->Log로 이름 변경

--- app/src/main/java/com/example/water_drinking_whale/data/main/repository/LogRepository.kt
+++ app/src/main/java/com/example/water_drinking_whale/data/main/repository/LogRepository.kt
@@ -1,17 +1,17 @@
 package com.example.water_drinking_whale.data.main.repository
 
-import com.example.water_drinking_whale.data.main.api.RecordApi
+import com.example.water_drinking_whale.data.main.api.LogApi
 import com.example.water_drinking_whale.data.main.model.MonthRecordResponse
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class RecordRepository {
+class LogRepository {
 
     @Singleton
     class RecordRepository @Inject constructor(
-        private val recordApi: RecordApi
+        private val logApi: LogApi
     ) {
 
-        suspend fun getMonthRecord(): MonthRecordResponse = recordApi.getMonthRecord()
+        suspend fun getMonthRecord(): MonthRecordResponse = logApi.getMonthRecord()
     }
 }
